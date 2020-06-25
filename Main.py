316d7c4ba6c3b674071a6daa20963a24c74e9792
Randy Weinstein <randy@mode755.com>
2020-06-25 18:40:41 -0400

    some more work

--- Main.py
+++ Main.py
@@ -10,29 +10,47 @@ debug: bool = True
 
 def main():
 
+    if debug:
+        print("debug mode ON")
+    else:
+        print("debug mode OFF")
     json_raw = get_response(query)
     json_parsed = json.loads(json_raw)
-    print(debug)
     if debug:
+        print("json_parsed: ")
         print(json_parsed)
 
-    #column names and metadata are stored in a parallel level as the data in the JSON
-    columnNames = []
-    columnDataTypes =[]
-    for field in json_parsed["fields"]:
-        columnNames.append(field["name"])
-        columnDataTypes.append(field["type"])
-        if debug:
-            print()
+    #column names and metadata are stored in a parallel level as the data in the JSON. We'll use this metadata to help parse the data
+    columns = get_columns(json_parsed)
+    if debug:
+        print("columns: ")
+        print(columns)
 
-    #now that we have column headers let's get the actual data
-    dataRowsJson = json_parsed["features"]
-    for row in dataRowsJson:
-        row_values = row["attributes"]
-        print(row_values)
-        print(type(row_values))
+    data_array = get_rows(json_parsed)
+    if debug:
+        print("data_array: ")
+        print(data_array)
 
     #df = pd.read_json(path_or_buf=dataRowsJson, orient="records")
+
+
+def get_rows(json:dict):
+    rows = []
+    rowsJson = json["features"]
+    for rowJson in rowsJson:
+        row = []
+        row_json:dict = rowJson["attributes"]
+        row_values:tuple = row_json.items()
+        for row_value in row_values:
+            print(row_value)
+            print(type(row_value))
+    return rows
+
+def categorical_to_ordinal_or_numerical(row: dict, ):
+
+    values_array = []
+
+
 
 
 
@@ -42,6 +60,9 @@ def main():
 # Based on the level of difficultly people had installing numpy & pandas, I decided to just circumvent the necessity by
 # using _create_unverified_context().  This prints an error on a non 200 HTTP response code.
 def get_response(query):
+    if(debug):
+        print("calling KSI API with the folling URL: ")
+        print(query)
     context = ssl._create_unverified_context()
     open_url = urllib.request.urlopen(query, context=context)
     if open_url.getcode() == 200:
@@ -49,5 +70,13 @@ def get_response(query):
     else:
         print("Error receiving data", open_url.getcode())
     return data
+
+def get_columns(json:dict):
+    columns:dict = {}
+    for field in json["fields"]:
+        name = field["name"]
+        sql_type:str = str(field["type"])
+        columns[name] = sql_type[13:]
+    return columns
 
 main()
