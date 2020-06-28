064bcdaa6b301571215cf6180681612f7aa9b7f8
Randy Weinstein <randy@mode755.com>
2020-06-28 11:16:56 -0400

    documentation

--- Main.py
+++ Main.py
@@ -7,8 +7,15 @@ import datetime
 
 debug = True
 
+# Internal note to Randy:
+# To update the docs do the following:
+# $ pipx install mkdocs
+# $ pydoc-markdown --bootstrap-mkdocs
+# $ pydoc-markdown --server --open-browser
+# cp /build/docs/content/api-documentation.md /README.md
 
 def main():
+    """This main()function is all that anyone working on this project should need to alter. This provides sample code on how to retrieve a Pandas DataFrame from a KSIFeed object, and how to retrieve the column mapping for categorical data"""
     if debug:
         print("debug mode ON")
     else:
@@ -17,23 +24,31 @@ def main():
     ksi = KSIFeed(index_start=1, index_end=3389167)
 
     if debug:
+        # if you want to see what API call was made
         print("Query:")
         print(ksi.get_query())
 
+        # if you want to see what the API returned
         print("Json:")
         print(ksi.get_json());
 
+        # if you want to see the legend for mapped categorical values
         print("Column Mapping:")
         print(ksi.get_column_mapper())
 
+    # the main call. Here is your Pandas DataFrame
     df = ksi.get_data_frame()
 
     print(df.dtypes)
     print(df)
 
-
+# it is safe to ignore anything past this line.  I will own this code
 class KSIFeed:
-
+    """This is the main object for calling the KSI API and retrieving a Pandas DataFrame object.
+    You may also retrieve the ColumnMapper from this object to see how the values were mapped.
+    :argument index_start the starting index of the values we want to retrieve, defaults to 0
+    :argument index_end largest index of the values we want to retrive, defaults to 3389167,
+    which gives us 167 rows at present."""
     def __init__(self, index_start: int = 1, index_end: int = 3389167):
         self._query: str = "https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/KSI/FeatureServer/0/query?where=Index_%20%3E%3D%20" + str(
             index_start) + "%20AND%20Index_%20%3C%3D%20" + str(index_end) + "&outFields=*&outSR=4326&f=json"
@@ -51,22 +66,27 @@ class KSIFeed:
             rows_json: dict = rows_json["attributes"]
             row_json = rows_json.items()
             for value in row_json:
-                row.append(self._column_mapper.get_int_for_value(value))
+                row.append(self._column_mapper.transform_value(value))
             self._rows.append(row)
 
     def get_json(self):
+        """:returns the json that the API returned, already parsed in Python"""
         return self._json_parsed
 
     def get_query(self):
+        """:returns the query that this object called the API with. Cut and paste into a browser to see the raw data"""
         return self._query
 
     def get_rows(self):
+        """:returns list a python 2 dimensional list of mixed datatypes in consistent order"""
         return self._rows
 
     def get_column_mapper(self):
+        """:returns ColumnMapper object"""
         return self._column_mapper
 
     def get_data_frame(self):
+        """:returns Pandas DataFrame. This is the main method of this class"""
         df = pd.DataFrame(data=self._rows, columns=self._column_mapper.get_column_names())
         return df
 
@@ -75,11 +95,8 @@ class KSIFeed:
         Installing certs in Python to open SSL connections is challenging.
         Based on the level of difficultly people had installing numpy & pandas, I decided to just circumvent the necessity by
         using _create_unverified_context().  This prints an error on a non 200 HTTP response code.
-        :return:
+        :return: raw json
         '''
-        if (debug):
-            print("calling KSI API with the following URL: ")
-            print(query)
         context = ssl._create_unverified_context()
         open_url = urllib.request.urlopen(query, context=context)
         if open_url.getcode() == 200:
@@ -90,12 +107,23 @@ class KSIFeed:
 
 
 class ColumnType:
+    """The KSI data feed has two significant problems that this object tries to address:
+    1. Objects are stored and returned as a string, even if the data is a DateTime, Integer, Float, or Boolean value
+    2. For categorical values, there is no list of possible values in the KSI documentation
+    Therefore, this object has two main functions:
+    1. Parse the appropriate Python datatype for the column
+    2. Maintain an internal map of Integer values for categorical data
+    In the case that we want the Integer values to be Ordinal, we will have to modify this code so that the ColumnMapper is pre-populated with some subclassess of ColumnType where necessary"""
     def __init__(self, name: str, datatype: str):
         self.name: str = name
         self.datatype: str = datatype
         self.possible_values: dict = dict()
 
     def __str__(self):
+        """This returns a string representation of the ColumnType
+        If it is called before the data is parsed, categorical data will not yet be mapped
+        Therefore, this should be accessed via the __str__() method of the ColumnMapper object as the categorical data
+        is mapped on ColumnMapper creation and will always return the full mapping for categorical data"""
         current_map = "ColumnType Object: \n"
         current_map += "\tName: " + self.name + "\n"
         current_map += "\tType: " + self.datatype + "\n"
@@ -104,6 +132,20 @@ class ColumnType:
         return current_map
 
     def transform_value(self, value: str):
+        """This is the "meat" of the project. The logic is as follows:
+        1. None values are returned as is
+        2. Columns with sql datatype "Integer" are returned as a Python int
+        3. Columns with sql datatype "Date" are returned as a Python int
+        4. Columns with sql datatype "OID" are returned as a Python int
+        5. Columns with sql datatype "String" require the following logic:
+            - "Yes" or "No" are retuyned as a Python bool
+            - if the number looks like an Integer, return a Python int
+            - if the number looks like an Float, return a Python float
+            - if the number is a categorical value, ie it did not pass the previous checks, then
+            build a map of the values as we encounter them and return a Python int. The value of the int
+            represents the order in which the categorical value appeared in the feed, these are not ordinal values.
+            In order to return ordinal values, subclass this object and pre-populate the ColumnMapper with an instance
+            of this subclass"""
         # don't wast time on null values.  Just return the null value
         if value is None:
             return value
@@ -144,6 +186,7 @@ class ColumnType:
 
 
 class ColumnMapper:
+    '''This object builds a list of ColumnType objects from the "fields" section of the JSON. Use this object to interact with ColumnTypes instead of instatiating ColumnTypes directly'''
     def __init__(self, columns_json: dict):
         self.columns: dict = dict()
         for field in columns_json["fields"]:
@@ -153,19 +196,27 @@ class ColumnMapper:
             self.columns[name] = ColumnType(name, formatted_type)
 
     def __str__(self):
+        """Prints out the current list of ColumnTypes
+        :returns str"""
         current_map: str = "ColumnMapper Object\n"
         for column_type in self.columns.values():
             current_map += str(column_type)
         return current_map
 
-    def get_int_for_value(self, value: tuple):
+    def transform_value(self, value: tuple):
+        """Convenience method to look up the correct ColumnType for a value
+        :argument tuple ( name of column , column value to transform
+        :returns the transformed value in the python data type that makes the most sense as per the
+        corresponding ColumnType object's transform_value() method"""
         column_type: ColumnType = self.columns[value[0]]
-        return column_type.transform_value(value[1]);
+        return column_type.transform_value(value[1])
 
     def get_column_types(self):
+        """:returns list a list of all the ColumnTypes this object found inj the "fields" array of the JSON on init."""
         return self.columns
 
     def get_column_names(self):
+        """:returns list a list of all the column names, useful in creating a pandas DataFrame"""
         return self.columns.keys()
 
 
--- README.md
+++ README.md
@@ -1,11 +1,153 @@
-# YorkUBigDataClass1
+<a name=".Main"></a>
+# Main
 
-## Overview
+<a name=".Main.main"></a>
+#### main
 
-This python script reads data from  the Toronto Police Killed or [Seriously Injured dataset (KSI)](https://data.torontopolice.on.ca/datasets/ksi) via a rest API call and populates a dataframe, substituting categorical data where it makes sense
+```python
+main()
+```
 
+This main()function is all that anyone working on this project should need to alter. This provides sample code on how to retrieve a Pandas DataFrame from a KSIFeed object, and how to retrieve the column mapping for categorical data
 
-## Data subs
+<a name=".Main.KSIFeed"></a>
+## KSIFeed Objects
 
+```python
+class KSIFeed():
+ |  KSIFeed(index_start: int = 1, index_end: int = 3389167)
+```
 
+This is the main object for calling the KSI API and retrieving a Pandas DataFrame object.
+You may also retrieve the ColumnMapper from this object to see how the values were mapped.
+:argument index_start the starting index of the values we want to retrieve, defaults to 0
+:argument index_end largest index of the values we want to retrive, defaults to 3389167,
+which gives us 167 rows at present.
+
+<a name=".Main.KSIFeed.get_json"></a>
+#### get\_json
+
+```python
+ | get_json()
+```
+
+:returns the json that the API returned, already parsed in Python
+
+<a name=".Main.KSIFeed.get_query"></a>
+#### get\_query
+
+```python
+ | get_query()
+```
+
+:returns the query that this object called the API with. Cut and paste into a browser to see the raw data
+
+<a name=".Main.KSIFeed.get_rows"></a>
+#### get\_rows
+
+```python
+ | get_rows()
+```
+
+:returns list a python 2 diment
+
+<a name=".Main.ColumnType"></a>
+## ColumnType Objects
+
+```python
+class ColumnType():
+ |  ColumnType(name: str, datatype: str)
+```
+
+The KSI data feed has two significant problems that this object tries to address:
+1. Objects are stored and returned as a string, even if the data is a DateTime, Integer, Float, or Boolean value
+2. For categorical values, there is no list of possible values in the KSI documentation
+Therefore, this object has two main functions:
+1. Parse the appropriate Python datatype for the column
+2. Maintain an internal map of Integer values for categorical data
+In the case that we want the Integer values to be Ordinal, we will have to modify this code so that the ColumnMapper is pre-populated with some subclassess of ColumnType where necessary
+
+<a name=".Main.ColumnType.__str__"></a>
+#### \_\_str\_\_
+
+```python
+ | __str__()
+```
+
+This returns a string representation of the ColumnType
+If it is called before the data is parsed, categorical data will not yet be mapped
+Therefore, this should be accessed via the __str__() method of the ColumnMapper object as the categorical data
+is mapped on ColumnMapper creation and will always return the full mapping for categorical data
+
+<a name=".Main.ColumnType.transform_value"></a>
+#### transform\_value
+
+```python
+ | transform_value(value: str)
+```
+
+This is the "meat" of the project. The logic is as follows:
+1. None values are returned as is
+2. Columns with sql datatype "Integer" are returned as a Python int
+3. Columns with sql datatype "Date" are returned as a Python int
+4. Columns with sql datatype "OID" are returned as a Python int
+5. Columns with sql datatype "String" require the following logic:
+    - "Yes" or "No" are retuyned as a Python bool
+    - if the number looks like an Integer, return a Python int
+    - if the number looks like an Float, return a Python float
+    - if the number is a categorical value, ie it did not pass the previous checks, then
+    build a map of the values as we encounter them and return a Python int. The value of the int
+    represents the order in which the categorical value appeared in the feed, these are not ordinal values.
+    In order to return ordinal values, subclass this object and pre-populate the ColumnMapper with an instance
+    of this subclass
+
+<a name=".Main.ColumnMapper"></a>
+## ColumnMapper Objects
+
+```python
+class ColumnMapper():
+ |  ColumnMapper(columns_json: dict)
+```
+
+This object builds a list of ColumnType objects from the "fields" section of the JSON. Use this object to interact with ColumnTypes instead of instatiating ColumnTypes directly
+
+<a name=".Main.ColumnMapper.__str__"></a>
+#### \_\_str\_\_
+
+```python
+ | __str__()
+```
+
+Prints out the current list of ColumnTypes
+:returns str
+
+<a name=".Main.ColumnMapper.transform_value"></a>
+#### transform\_value
+
+```python
+ | transform_value(value: tuple)
+```
+
+Convenience method to look up the correct ColumnType for a value
+:argument tuple ( name of column , column value to transform
+:returns the transformed value in the python data type that makes the most sense as per the
+corresponding ColumnType object's transform_value() method
+
+<a name=".Main.ColumnMapper.get_column_types"></a>
+#### get\_column\_types
+
+```python
+ | get_column_types()
+```
+
+:returns list a list of all the ColumnTypes this object found inj the "fields" array of the JSON on init.
+
+<a name=".Main.ColumnMapper.get_column_names"></a>
+#### get\_column\_names
+
+```python
+ | get_column_names()
+```
+
+:returns list a list of all the column names, useful in creating a pandas DataFrame
 
